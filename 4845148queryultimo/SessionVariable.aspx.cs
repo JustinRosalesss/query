c229719c3f9d72f028c8eaeaf93f18e43b4337b4
justin <justinrulesxdxd@gmail.com>
2025-03-06 09:48:23 -0600

    primer commit

---
+++ 4845148queryultimo/SessionVariable.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _4845148queryultimo
+{
+    public partial class SessionVariable : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnConfirm_Click(object sender, EventArgs e)
+        {
+            Session["ddlCategory"] = ddlCategory.SelectedValue;
+            Session["ddlSupplier"] = ddlSupplier.SelectedValue;
+            Session["strProduct"] = txtProduct.Text;
+            Session["strDescription"] = TextArea1.InnerText;
+            Session["strImage"] = txtImage.Text;
+            Session["decPrice"] = txtPrice.Text;
+            Session["bytNumberInStock"] = txtNumberInStock.Text;
+            Session["bytNumberOnOrder"] = txtNumberOnOrder.Text;
+            Session["bytReorderLevel"] = txtReorderLevel.Text;
+
+            Response.Redirect("ProductConfirmSessionVariable.aspx");
+        }
+    }
+}
